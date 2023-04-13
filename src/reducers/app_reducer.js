8ce3e532fd36451c136dbc04348fa170b4317726
guangnian <1300303386@qq.com>
2023-04-13 23:36:06 +0800

    取消严格模式

--- src/reducers/app_reducer.js
+++ src/reducers/app_reducer.js
@@ -4,11 +4,9 @@ const initState = {
 };
 function app_reducer(preState = initState, action) {
   const { type, data } = action;
-    console.log(action)
     return produce(preState, (newState) => {
       switch (type) {
         case "APP_REDUCER_SIZE_ADD":
-          console.log("@@@");
           newState.size += data;
           break;
         case "APP_REDUCER_SIZE_SUB":
